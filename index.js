42a870765cfaf77d9b1701b3e1f33844932f61fe
the-tree-house <109871372+treehouseapps@users.noreply.github.com>
2025-02-24 20:55:31 +0300

    update

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 const express = require('express')
 const app = express()
+const multer = require("multer");
+const path = require("path");
+const mongoose = require('mongoose')
+
+require('dotenv').config()
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/view')
 app.use(express.urlencoded({ extended: true }))
+app.use(express.static("public"));
+app.use('/uploads', express.static('uploads'));
 
-const mongoose = require('mongoose')
 
+const storage = multer.diskStorage({
+    destination: "./uploads/",
+    filename: (req, file, cb) => {
+        cb(null, file.fieldname + "-" + Date.now() + path.extname(file.originalname));
+    }
+});
+const upload = multer({ storage: storage });
 const schema = new mongoose.Schema({
     name: {
         type: String
@@ -15,6 +28,9 @@ const schema = new mongoose.Schema({
     },
     desc: {
         type: String
+    },
+    image: {
+        type: String
     }
 })
 const collection = new mongoose.model('sites', schema)
@@ -23,11 +39,14 @@ app.get('/', async (req, res) => {
     const result = await collection.find()
     res.render('index', { result })
 })
-app.post('/add', async (req, res) => {
-    await collection.insertMany(req.body)
+app.post('/add', upload.single("image"), async (req, res) => {
+    const data = {
+        ...req.body,
+        image: req.file ? req.file.filename : null
+    }
+    await collection.insertMany(data)
     res.redirect('/')
 })
-
 app.get('/delete/:id', async (req, res) => {
     await collection.deleteOne({ _id: req.params.id })
     res.redirect('/')
@@ -35,7 +54,7 @@ app.get('/delete/:id', async (req, res) => {
 app.get('*', (req, res) => {
     res.render('404')
 })
-mongoose.connect('mongodb+srv://Beki:78122775Beki@cluster0.6ypmi.mongodb.net/sites')
+mongoose.connect(process.env.DBCONNECTION)
     .then(() => {
         app.listen(process.env.PORT, () => {
             console.log('Server Running in port ' + process.env.PORT)
