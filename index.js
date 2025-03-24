34e3e133be415173964ca30555a4b134c25f7ebe
Beki Junior <bbekijunior1@gmail.com>
2025-03-25 00:32:01 +0300

    update

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
+const session = require("express-session");
 
 require('dotenv').config()
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/view')
 app.use(express.urlencoded({ extended: true }))
 
+app.use(
+    session({
+        secret: process.env.SESSION_SECRET,
+        cookie: { maxAge: 200000 },
+        resave: false,
+        saveUninitialized: false,
+    })
+);
 const schema = new mongoose.Schema({
     name: {
         type: String
@@ -22,14 +31,69 @@ const collection = new mongoose.model('sites', schema)
 
 app.get('/', async (req, res) => {
     const result = await collection.find()
-    res.render('index', { result })
+    if (req.session) {
+        res.render('index', { result, session: req.session.name })
+    }
+    else {
+        res.render('index', { result })
+    }
 })
 app.post('/add', async (req, res) => {
-    await collection.insertMany(req.body);
-    res.redirect('/');
+    try {
+        if (req.session.name == 'treehouse') {
+            await collection.insertMany(req.body);
+            res.redirect('/');
+        }
+        else {
+            res.redirect('/');
+        }
+    }
+    catch {
+        res.redirect('/')
+    }
+})
+app.post('/login', (req, res) => {
+    try {
+        req.session.name = req.body.name
+        res.redirect('/')
+    }
+    catch {
+        res.redirect('/')
+    }
+})
+app.post('/update/:id', async (req, res) => {
+    try {
+        if (req.session.name == 'treehouse') {
+            let id = req.params.id;
+            let updatedData = req.body
+            console.log(req.body)
+            await collection.findByIdAndUpdate(id, updatedData, { new: true });
+            res.redirect('/')
+        }
+        else {
+            res.redirect('/')
+        }
+    }
+    catch {
+        res.redirect('/');
+    }
 })
 app.get('/delete/:id', async (req, res) => {
-    await collection.deleteOne({ _id: req.params.id })
+    try {
+        if (req.session.name == 'treehouse') {
+            await collection.deleteOne({ _id: req.params.id })
+            res.redirect('/');
+        }
+        else {
+            res.redirect('/');
+        }
+    }
+    catch {
+        res.redirect('/')
+    }
+})
+app.get('/logout', (req, res) => {
+    req.session.destroy()
     res.redirect('/')
 })
 app.get('*', (req, res) => {
