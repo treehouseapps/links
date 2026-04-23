e3a74d79da2dd4452f950f22f5cb90d00fe94228
treehouseapps <bbekijunior1@gmail.com>
2026-04-23 15:05:17 +0300

    fixing

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const session = require("express-session");
 
 require("dotenv").config();
+
 app.set("view engine", "ejs");
 app.set("views", __dirname + "/view");
 app.use(express.urlencoded({ extended: true }));
@@ -11,45 +12,50 @@ app.use(express.urlencoded({ extended: true }));
 app.use(
   session({
     secret: process.env.SESSION_SECRET,
-    cookie: { maxAge: 200000 },
     resave: false,
     saveUninitialized: false,
+    cookie: { maxAge: 200000 },
   }),
 );
+
+mongoose
+  .connect(process.env.DBCONNECTION)
+  .then(() => console.log("Connected to DB"))
+  .catch((err) => console.log("DB error", err));
+
 const schema = new mongoose.Schema({
-  name: {
-    type: String,
-  },
-  link: {
-    type: String,
-  },
-  desc: {
-    type: String,
-  },
+  name: String,
+  link: String,
+  desc: String,
 });
-const collection = new mongoose.model("sites", schema);
+
+const collection = mongoose.model("sites", schema);
 
 app.get("/", async (req, res) => {
-  await connectDB();
-  const result = await collection.find();
-  if (req.session) {
-    res.render("index", { result, session: req.session.name });
-  } else {
-    res.render("index", { result });
+  try {
+    const result = await collection.find();
+
+    res.render("index", {
+      result,
+      session: req.session?.name || null,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("Database error");
   }
 });
+
 app.post("/add", async (req, res) => {
   try {
-    if (req.session.name == "treehouse") {
+    if (req.session.name === "treehouse") {
       await collection.insertMany(req.body);
-      res.redirect("/");
-    } else {
-      res.redirect("/");
     }
+    res.redirect("/");
   } catch {
     res.redirect("/");
   }
 });
+
 app.post("/login", (req, res) => {
   try {
     req.session.name = req.body.name;
@@ -58,51 +64,36 @@ app.post("/login", (req, res) => {
     res.redirect("/");
   }
 });
+
 app.post("/update/:id", async (req, res) => {
   try {
-    if (req.session.name == "treehouse") {
-      let id = req.params.id;
-      let updatedData = req.body;
-      await collection.findByIdAndUpdate(id, updatedData, { new: true });
-      res.redirect("/");
-    } else {
-      res.redirect("/");
+    if (req.session.name === "treehouse") {
+      await collection.findByIdAndUpdate(req.params.id, req.body);
     }
+    res.redirect("/");
   } catch {
     res.redirect("/");
   }
 });
+
 app.get("/delete/:id", async (req, res) => {
   try {
-    if (req.session.name == "treehouse") {
+    if (req.session.name === "treehouse") {
       await collection.deleteOne({ _id: req.params.id });
-      res.redirect("/");
-    } else {
-      res.redirect("/");
     }
+    res.redirect("/");
   } catch {
     res.redirect("/");
   }
 });
+
 app.get("/logout", (req, res) => {
   req.session.destroy();
   res.redirect("/");
 });
+
 app.get("*", (req, res) => {
   res.render("404");
 });
-let isConnected = false;
-
-const connectDB = async () => {
-  if (isConnected) return;
-
-  try {
-    await mongoose.connect(process.env.DBCONNECTION);
-    isConnected = true;
-    console.log("Connected to DB");
-  } catch (err) {
-    console.log("DB connection error");
-  }
-};
 
 module.exports = app;
